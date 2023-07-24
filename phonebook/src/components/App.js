c4ffed97a83d6cde4a942da31260d691939252da
Luis Sanchez <luis.sanchez.dev@gmail.com>
2023-07-24 18:15:38 -0500

    solved exercises 2.11

--- phonebook/src/components/App.js
+++ phonebook/src/components/App.js
@@ -1,15 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import Search from './Search';
 import FormAddPerson from './FormAddPerson';
 import InfoPerson from './InfoPerson';
 
 function App() {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' },
-  ]);
+  const [persons, setPersons] = useState([])
+
+  useEffect(() => {
+    console.log({persons})
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('server response')
+        setPersons(response.data)
+      })
+
+  }, [])
+
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [termFilter, setTermFilter] = useState('');
